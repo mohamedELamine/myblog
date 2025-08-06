08a82be7bba440145c7a134d7988c0a0a20ccf10
MOHAMMED <mohammed@MOHAMMEDs-MacBook-Air.local>
2025-08-06 20:58:36 +0100

    تعديل المحتوى

--- pages/404.tsx
+++ pages/404.tsx
@@ -15,17 +15,17 @@ export default function NotFoundPage() {
       <NavBar />
       <ContentContainer>
         <h2 className={`my-5 flex justify-center text-2xl font-bold font-fang-zheng-xiao-biao-song`}>
-          {"404 NOT FOUND"}
+          {"404 ما تطلبه غير موجود"}
         </h2>
         <Separator />
         <div className="my-5 flex flex-col justify-center">
           <TfiFaceSad className="mx-auto my-4" size={"6em"} />
           <p className={`font-source-serif-screen mx-auto my-3 text-center text-xl`}>
-            {"This page does not exist for it might be removed or closed."}
+            {"عذرًا، الصفحة التي تبحث عنها غير موجودة. ربما تم حذفها أو تغيير عنوانها."}
           </p>
           <div className="my-5 flex justify-center">
             <Button className="font-bold" onClick={handleGoBack}>
-              {"GO BACK"}
+              {"العودة للصفحة السابقة"}
             </Button>
           </div>
         </div>
